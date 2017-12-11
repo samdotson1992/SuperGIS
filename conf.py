fb2a3f6b86c41405380fbf6e6d041e4871492d70
Samuel Dotson <samdotson1992@gmail.com>
2017-12-11 08:34:39 -0600

    Nikola auto commit.
    Nikola version: 7.8.10

--- conf.py
+++ conf.py
@@ -315,7 +315,7 @@ COMPILERS = {
 # Nikola supports logo display.  If you have one, you can put the URL here.
 # Final outpuITLE">.
 # The URL may be relative to the site root.
-LOGO_URL = "logo.png" 
+LOGO_URL = "Asset 5-100.jpg" 
 
 # If you want to hide the title of your website (for example, if your logo
 # already contains the text), set t     his to False.
